225af601a1ee184dce41183d057cc682440cf7cc
passivist <raffaelseyfried@gmail.com>
2016-01-10 14:14:03 +0100

    add source files

---
+++ Source/Grain.h
@@ -0,0 +1,22 @@
+//
+//  Grain.h
+//  GRNLR_klein
+//
+//  Created by laborant on 08.01.16.
+//
+//
+
+#ifndef GRAIN_H_INCLUDED
+#define GRAIN_H_INCLUDED
+
+#include <vector>
+
+class grainEnvelope {
+
+};
+
+class Grain {
+    
+};
+
+#endif /* Grain_h */
