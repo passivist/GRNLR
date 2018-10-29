8558f92621cdd8b8e362003ee9cd2f5c68a41101
Raffael Seyfried <raffaelseyfried@gmail.com>
2018-10-29 22:42:22 +0100

    Update windows Plugin
    Updated the windows plugin and projucer project

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -10,8 +10,7 @@
 
 */
 
-#ifndef __APPHEADERFILE_RJZQML__
-#define __APPHEADERFILE_RJZQML__
+#pragma once
 
 #include "AppConfig.h"
 
@@ -46,5 +45,3 @@ namespace ProjectInfo
     const int          versionNumber  = 0x10000;
 }
 #endif
-
-#endif   // __APPHEADERFILE_RJZQML__
